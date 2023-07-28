cacbf846f8fd469520f27a38712c97dc37c2a5b1
rkvimal <55201528+rkvimal@users.noreply.github.com>
2023-07-28 14:31:52 +0530

    DB connection Closed after processing
    DB connection Closed after getting the required data

--- sources/Test.py
+++ sources/Test.py
@@ -34,6 +34,13 @@ print("Connection with prod mysql established")
 df = pd.read_sql("select r.expresso_id,date(r.order_create_date) as Order_Date, concat(COALESCE(u2.first_name,''), ' ' , COALESCE(u2.last_name,'')) as Sales_User, concat(COALESCE(u.first_name,''), ' ' , COALESCE(u.last_name,'')) as OPS_User, case r.status when 1 then 'Waiting for submission' when 2 then 'Pending' when 3 then 'Rejected' when 4 then 'Recalled' when 5 then 'Approved' when 100 then 'Sales User Review Required' when 109 then 'Value Add Approval Required' when 203 then 'CS Approval Required' when 210 then 'IRO Approved' when 211 then 'Campaign Created' when 215 then 'Finance Review Required For Campaign Close' when 321 then 'Invoice Generated' when 333 then 'Completed' else 'Others' end as Campaign_Status from til_ro r join til_ro_lineitem rl on r.til_ro_id = rl.til_ro_id join til_lineitem l on rl.til_lineitem_id = l.til_lineitem_id join til_user u on u.til_user_id = l.til_user_id_ops join til_user u2 on u2.til_user_id = r.til_user_id_sales where r.demand_channel = 0 and DATEDIFF(SYSDATE(), r.order_create_date) <= 7 AND r.company_code = 6200 AND r.til_user_id_sales IN (5243,28359,43415,45287,47047,52588,54494,57925,58311,58733,59724,60589,64672,65964,66308,66479,67194,67448,67498,68263,68265,68268,68270,68271,68273,68806,69482,69551,69607,69639,69679,69680,69799,70304,70305,70374,70413,70617,70866,70873,71282,71289,71574,71575,71576,71664) group by 1,2,3,4,5 order by 5",prod_connection)
 df1 = pd.read_sql("select case r.status when 1 then 'Waiting for submission' when 2 then 'Pending' when 3 then 'Rejected' when 4 then 'Recalled' when 5 then 'Approved' when 100 then 'Sales User Review Required' when 109 then 'Value Add Approval Required' when 203 then 'CS Approval Required' when 210 then 'IRO Approved' when 211 then 'Campaign Created' when 215 then 'Finance Review Required For Campaign Close' when 321 then 'Invoice Generated' when 333 then 'Completed' else 'Others' end as Campaign_Status, count(distinct r.expresso_id) as RO_Count from til_ro r where r.demand_channel = 0 and DATEDIFF(SYSDATE(), r.order_create_date) <= 7 AND r.company_code = 6200 AND r.til_user_id_sales IN (5243,28359,43415,45287,47047,52588,54494,57925,58311,58733,59724,60589,64672,65964,66308,66479,67194,67448,67498,68263,68265,68268,68270,68271,68273,68806,69482,69551,69607,69639,69679,69680,69799,70304,70305,70374,70413,70617,70866,70873,71282,71289,71574,71575,71576,71664) group by 1 order by 2 desc",prod_connection)
 
+# Close the MySQL connection
+prod_connection.close()
+print("Connection with prod MySQL closed")
+
+# Close the SSH tunnel
+tunnel.stop()
+print("SSH tunnel closed")
 
 
 # Convert DataFrame df1 to an HTML table
